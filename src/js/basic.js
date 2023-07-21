f76dcbc47c47f83c54acafb55c9d3068f3a219e7
GLM-Alyona <glmalyona@gmail.com>
2023-07-22 00:49:18 +0300

    Person already exists

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-dupe-class-members */
 export default class Team {
   constructor() {
     this.members = new Set();
   }
 
   add(player) {
-    this.members.add(player);
+    if (this.members.has(player)) {
+      throw new Error('Person already exists!');
+    } else {
+      this.members.add(player);
+    }
   }
 
   addAll(...players) {
